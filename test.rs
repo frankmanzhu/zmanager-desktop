48b9ffd8feb2ee2e90addd79cc5b25c139ce315c
frankzhu <frankmanzhu@hotmail.com>
2026-07-24 16:21:16 +1000

    chore: remove accidentally committed temporary scripts, binaries, and leftover QA notes

--- test.rs
+++
@@ -1,8 +0,0 @@
-use std::path::Path;
-
-fn main() {
-    println!("{:?}", Path::new(".").file_name());
-    println!("{:?}", Path::new("/").file_name());
-    println!("{:?}", Path::new("/home/frank/beeware-tutorial/").file_name());
-    println!("{:?}", Path::new("beeware-tutorial").file_name());
-}
